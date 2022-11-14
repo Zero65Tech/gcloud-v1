1849f2a956233fcd8f9ef641ad654c89a9fe30a0
Prashant <prashant@zero65.in>
2022-11-14 16:21:07 +0530

    /github-webhook - pull from github, build and push to artifacts registry

--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const app     = express();
 
+const { CloudBuildClient } = require('@google-cloud/cloudbuild').v1;
+
+app.use(express.json());
+
+const CloudBuild = new CloudBuildClient();
+
 
 
 app.get('/', async (req, res) => {
-  res.send('Hello NodeJs !');
+  res.send('Hello GCloud !');
 });
 
-app.get('/_env', async (req, res) => {
-  res.send(process.env);
+app.post('/github-webhook', async (req, res) => {
+
+  let projectId = 'zero65';
+  let region = 'asia-southeast1';
+  let registry = 'docker';
+  let repository = req.body.repository;
+  let commit = req.body.head_commit;
+
+  const request = {
+    projectId: projectId,
+    build: {
+      "steps": [
+        {
+          name: 'gcr.io/cloud-builders/git',
+          args: [ 'clone', repository.clone_url ]
+        },
+        {
+          name: 'gcr.io/cloud-builders/docker',
+          args: [
+            'build',
+            '-t', `${ region }-docker.pkg.dev/${ projectId }/${ registry }/${ repository.name }:${ commit.id }`,
+            '-t', `${ region }-docker.pkg.dev/${ projectId }/${ registry }/${ repository.name }:latest`,
+            '-f', `${ repository.name }/Dockerfile`,
+            repository.name ]
+        },
+        {
+          name: 'gcr.io/cloud-builders/docker',
+          args: ['push', `${ region }-docker.pkg.dev/${ projectId }/${ registry }/${ repository.name }:${ commit.id }`]
+        },
+        {
+          name: 'gcr.io/cloud-builders/docker',
+          args: ['push', `${ region }-docker.pkg.dev/${ projectId }/${ registry }/${ repository.name }:latest`]
+        }
+      ]
+    }
+  };
+
+  await CloudBuild.createBuild(request);
+
+  res.send('Build created successfully !');
+
 });
 
 
